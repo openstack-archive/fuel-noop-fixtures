e52cff5c8aebee54b86afd0d085a5edc295c36f9
Dmitry Ilyin <dilyin@mirantis.com>
2016-02-24 19:53:55 +0300

    Update
    * Improve reporting
    * More comments
    * Port back file resource reporting features
    
    Change-Id: I218fce4e6012ce1171a5042eebc8ac95ba734c21

--- lib/noop/manager/setup.rb
+++ lib/noop/manager/setup.rb
@@ -1,59 +1,90 @@
 module Noop
   class Manager
 
+    # Get a GEM_HOME either from the environment (using RVM)
+    # or from the default value (using bundle)
+    # @return [Pathname]
     def dir_path_gem_home
       return Pathname.new ENV['GEM_HOME'] if ENV['GEM_HOME']
       dir_name_bundle = Pathname.new 'bundled_gems'
       Noop::Config.dir_path_workspace + dir_name_bundle
     end
 
+    # Check if bundle command is installed
+    # @return [true,false]
     def bundle_installed?
       `bundle --version`
       $?.exitstatus == 0
     end
 
+    # Check if librarian-puppet command is installed
+    # @return [true,false]
     def librarian_installed?
       `librarian-puppet version`
       $?.exitstatus == 0
     end
 
+    # Setup bundle in the fixtures repo and bundle for puppet librarian
     def setup_bundle
       ENV['GEM_HOME'] = dir_path_gem_home.to_s
-      Dir.chdir Noop::Config.dir_path_root
-      bundle_install_and_update
-      Dir.chdir Noop::Config.dir_path_deployment
-      bundle_install_and_update
+      bundle_install_and_update Noop::Config.dir_path_root
+      bundle_install_and_update Noop::Config.dir_path_deployment
       Dir.chdir Noop::Config.dir_path_root
     end
 
+    # Run update script to setup external Puppet modules
     def setup_library
       ENV['GEM_HOME'] = dir_path_gem_home.to_s
-      Dir.chdir Noop::Config.dir_path_deployment
-      update_puppet_modules
+      update_puppet_modules Noop::Config.dir_path_deployment
       Dir.chdir Noop::Config.dir_path_root
     end
 
-    def bundle_install_and_update
-      Noop::Utils.error 'Bundle is not installed!' unless bundle_installed?
-      Noop::Utils.debug "Starting 'bundle install' in the Gem home: #{ENV['GEM_HOME']}"
-      Noop::Utils.run 'bundle install'
-      Noop::Utils.error 'Could not prepare bundle environment!' if $?.exitstatus != 0
-      Noop::Utils.debug "Starting 'bundle update' in the Gem home: #{ENV['GEM_HOME']}"
-      Noop::Utils.run 'bundle update'
-      Noop::Utils.error 'Could not update bundle environment!' if $?.exitstatus != 0
+    # @return [Pathname]
+    def file_name_gemfile_lock
+      Pathname.new 'Gemfile.lock'
     end
 
-    # run librarian-puppet to fetch modules as necessary
-    def update_puppet_modules
-      Noop::Utils.error 'Puppet Librarian is not installed!' unless librarian_installed?
+    # Remove the Gem lock file at the given path
+    # @param root [String,Pathname]
+    def remove_gemfile_lock(root)
+      root = Noop::Utils.convert_to_path root
+      lock_file_path = root + file_name_gemfile_lock
+      if  lock_file_path.file?
+        debug "Removing Gem lock file: '#{lock_file_path}'"
+        lock_file_path.unlink
+      end
+    end
+
+    # Run bundles install and update actions in the given folder
+    # @param root [String,Pathname]
+    def bundle_install_and_update(root)
+      error 'Bundle is not installed!' unless bundle_installed?
+      root = Noop::Utils.convert_to_path root
+      remove_gemfile_lock root
+      Dir.chdir root or error "Could not chdir to: #{root}"
+      debug "Starting 'bundle install' at: '#{root}' with the Gem home: '#{ENV['GEM_HOME']}'"
+      Noop::Utils.run 'bundle install'
+      error 'Could not prepare bundle environment!' if $?.exitstatus != 0
+      debug "Starting 'bundle update' at: '#{root}' with the Gem home: '#{ENV['GEM_HOME']}'"
+      Noop::Utils.run 'bundle update'
+      error 'Could not update bundle environment!' if $?.exitstatus != 0
+    end
+
+    # Run librarian-puppet to fetch modules as
+    # necessary modules at the given folder
+    # @param root [String,Pathname]
+    def update_puppet_modules(root)
+      error 'Puppet Librarian is not installed!' unless librarian_installed?
+      root = Noop::Utils.convert_to_path root
+      Dir.chdir root or error "Could not chdir to: #{root}"
       command = './update_modules.sh -v'
       command = command + ' -b' if options[:bundle_exec]
       command = command + ' -r' if options[:reset_librarian_puppet]
 
-      Noop::Utils.debug 'Starting update_modules script'
+      debug 'Starting update_modules script'
       Noop::Utils.run command
-      Noop::Utils.error 'Unable to update upstream puppet modules using librarian-puppet!' if $?.exitstatus != 0
-      Noop::Utils.debug 'Finished update_modules script'
+      error 'Unable to update upstream puppet modules using librarian-puppet!' if $?.exitstatus != 0
+      debug 'Finished update_modules script'
     end
 
   end
--- lib/noop/task/helpers.rb
+++ lib/noop/task/helpers.rb
@@ -1,46 +1,76 @@
 module Noop
   class Task
 
+    # Extract the parameter or property of a Puppet resource in the catalog
+    # @param context [RSpec::ExampleGroup] The 'self' of the RSpec example group
+    # @param resource_type [String] Name of the resource type
+    # @param resource_name [String] Title of the resource
+    # @param parameter [String] Parameter name
+    # @return [Object]
     def resource_parameter_value(context, resource_type, resource_name, parameter)
       catalog = context.subject
       catalog = catalog.call if catalog.is_a? Proc
       resource = catalog.resource resource_type, resource_name
-      fail "No resource type: '#{resource_type}' name: '#{resource_name}' in the catalog!" unless resource
+      error "No resource type: '#{resource_type}' name: '#{resource_name}' in the catalog!" unless resource
       resource[parameter.to_sym]
     end
 
-    # save the current puppet scope
+    # Save the current puppet scope
+    # @param value [Puppet::Scope]
     def puppet_scope=(value)
       @puppet_scope = value
     end
 
+    # The saved Puppet scope to run functions in
+    # Or the newly generated scope.
+    # @return [Puppet::Scope]
     def puppet_scope
       return @puppet_scope if @puppet_scope
       PuppetlabsSpec::PuppetInternals.scope
     end
 
-    # load a puppet function if it's not already loaded
+    # Load a puppet function if it's not already loaded
+    # @param name [String] Function name
     def puppet_function_load(name)
       name = name.to_sym unless name.is_a? Symbol
       Puppet::Parser::Functions.autoloader.load name
     end
 
-    # call a puppet function and return it's value
+    # Call a puppet function and return it's value
+    # @param name [String] Function name
+    # @param *args [Object] Function parameters
+    # @return [Object]
     def puppet_function(name, *args)
       name = name.to_sym unless name.is_a? Symbol
       puppet_function_load name
-      fail "Could not load Puppet function '#{name}'!" unless puppet_scope.respond_to? "function_#{name}".to_sym
+      error "Could not load Puppet function '#{name}'!" unless puppet_scope.respond_to? "function_#{name}".to_sym
       puppet_scope.send "function_#{name}".to_sym, args
     end
 
-    # take a variable value from the saved puppet scope
+    # Take a variable value from the saved puppet scope
+    # @param name [String] variable name
     def lookupvar(name)
       puppet_scope.lookupvar name
     end
+    alias :variable :lookupvar
 
-    # convert resource catalog to a RAL catalog
+    # Load a class from the Puppet modules into the current scope
+    # It can be used to extract values from 'params' classes like this:
+    # Noop.load_class 'nova::params'
+    # Noop.variable 'nova::params::common_package_name'
+    # => 'openstack-nova-common'
+    # These values can be later used in the spec examples.
+    # Note, that the loaded class will not be found in the spec's catalog
+    # object, but can be found here: Noop.puppet_scope.catalog
+    # @param class_name [String]
+    def load_class(class_name)
+      puppet_function 'include', class_name
+    end
+
+    # Convert resource catalog to a RAL catalog
     # and run both "generate" functions for each resource
     # that has it and then add results to the catalog
+    # @param context [RSpec::ExampleGroup] The 'self' of the RSpec example group
     # @return <Lambda>
     def create_ral_catalog(context)
       catalog = context.subject
