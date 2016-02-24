875eddc539142a9d3016184a8416f978b78aa87a
Dmitry Ilyin <dilyin@mirantis.com>
2016-02-24 19:53:55 +0300

    Update
    * Improve reporting
    * More comments
    * Port back file resource reporting features
    
    Change-Id: I218fce4e6012ce1171a5042eebc8ac95ba734c21

--- spec/shared-examples.rb
+++ spec/shared-examples.rb
@@ -4,17 +4,42 @@ end
 
 shared_examples 'show_catalog' do
   it 'shows catalog contents' do
-    puts '=' * 80
-    puts Noop.task.catalog_dump self
-    puts '=' * 80
+    Noop::Utils.output Noop::Utils.separator
+    Noop::Utils.output Noop.task.catalog_dump self
+    Noop::Utils.output Noop::Utils.separator
   end
 end
 
 shared_examples 'status' do
   it 'shows status' do
-    puts '=' * 80
-    puts Noop.task.status_report self
-    puts '=' * 80
+    Noop::Utils.output Noop::Utils.separator
+    Noop::Utils.output Noop.task.status_report self
+    Noop::Utils.output Noop::Utils.separator
+    Noop::Utils.output Noop.task.gem_versions_report
+    Noop::Utils.output Noop::Utils.separator
+  end
+end
+
+shared_examples 'files_installed_by_puppet' do
+  it 'should check that binary files are not installed by this task' do
+    Noop.catalog_file_resources_check self
+  end
+end
+
+shared_examples 'save_files_list' do
+  it 'should save the list of File resources to the file' do
+    Noop.catalog_file_report_write self
+  end
+end
+
+shared_examples 'saved_catalog' do
+  it 'should save the current task catalog to the file', :if => (ENV['SPEC_CATALOG_CHECK'] == 'save') do
+    Noop.file_write_task_catalog self
+  end
+  it 'should check the current task catalog against the saved one', :if => (ENV['SPEC_CATALOG_CHECK'] == 'check')  do
+    saved_catalog = Noop.preprocess_catalog_data Noop.file_read_task_catalog
+    current_catalog = Noop.preprocess_catalog_data Noop.catalog_dump self
+    expect(saved_catalog).to eq current_catalog
   end
 end
 
@@ -68,6 +93,9 @@ def run_test(manifest_file, *args)
   include_examples 'status' if ENV['SPEC_SHOW_STATUS']
   include_examples 'show_catalog' if ENV['SPEC_CATALOG_SHOW']
   include_examples 'console' if ENV['SPEC_RSPEC_CONSOLE']
+  include_examples 'files_installed_by_puppet' if ENV['SPEC_PUPPET_BINARY_FILES']
+  include_examples 'save_files_list' if ENV['SPEC_SAVE_FILE_RESOURCES']
+  include_examples 'saved_catalog' if ENV['SPEC_CATALOG_CHECK']
 
   begin
     include_examples 'catalog'
