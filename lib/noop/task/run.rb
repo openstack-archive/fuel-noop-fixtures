627487ecf46e26cf78cd8bb8c2a9ffb6c2811f68
Dmitry Ilyin <dilyin@mirantis.com>
2016-02-24 19:53:55 +0300

    Update
    * Improve reporting
    * More comments
    * Port back file resource reporting features
    
    Change-Id: I218fce4e6012ce1171a5042eebc8ac95ba734c21

--- lib/noop/task/run.rb
+++ lib/noop/task/run.rb
@@ -2,23 +2,24 @@ require 'json'
 
 module Noop
   class Task
+    # Run the actual spec of this task.
+    # It will execute the rspec command and will manage report files.
     def run
+      validate
+      error 'Validation of this task have failed!' unless valid?
       return unless pending?
       self.pid = Process.pid
       self.thread = Thread.current.object_id
-      Noop::Utils.debug "RUN: #{self.inspect}"
+      debug "RUN: #{self.inspect}"
       file_remove_report_json
       rspec_command_run
       file_load_report_json
       determine_task_status
-      Noop::Utils.debug "FINISH: #{self.inspect}"
+      debug "FINISH: #{self.inspect}"
       status
     end
 
-    def file_load_report_json
-      self.report = file_data_report_json
-    end
-
+    # Set the status string of the task according to run results
     def set_status_value(value)
       if value.is_a? TrueClass
         self.status = :success
@@ -29,6 +30,7 @@ module Noop
       end
     end
 
+    # Try to determine the task status based on the report data
     def determine_task_status
       if report.is_a? Hash
         failures = report.fetch('summary', {}).fetch('failure_count', nil)
@@ -39,45 +41,17 @@ module Noop
       status
     end
 
-    # @return [Pathname]
-    def file_name_report_json
-      Noop::Utils.convert_to_path "#{file_name_task_extension.sub_ext ''}_#{file_base_hiera}_#{file_base_facts}.json"
-    end
-
-    # @return [Pathname]
-    def file_path_report_json
-      Noop::Config.dir_path_reports + file_name_report_json
-    end
-
-    # @return [Hash]
-    def file_data_report_json
-      return unless file_present_report_json?
-      file_data = nil
-      begin
-        file_content = File.read file_path_report_json.to_s
-        file_data = JSON.load file_content
-        return unless file_data.is_a? Hash
-      rescue
-        nil
-      end
-      file_data
-    end
-
-    def file_remove_report_json
-      file_path_report_json.unlink if file_present_report_json?
-    end
-
-    # @return [true,false]
-    def file_present_report_json?
-      file_path_report_json.exist?
-    end
-
+    # Additional RSpec options
     def rspec_options
       options = '--color --tty'
       options += ' --format documentation' unless parallel_run?
       options
     end
 
+    # Run the RSpec command and pass Hiera, facts and spec files names
+    # using the environment variables.
+    # Use bundler if it's enabled.
+    # Set the task status according to the RSpec exit code.
     # @return [true,false]
     def rspec_command_run
       environment = {
@@ -89,10 +63,13 @@ module Noop
       command = "bundle exec #{command}" if ENV['SPEC_BUNDLE_EXEC']
       Dir.chdir Noop::Config.dir_path_root
       success = Noop::Utils.run environment, command
+      if success.nil?
+        debug 'RSpec command is not found!'
+        success = false
+      end
       set_status_value success
       success
     end
 
-    attr_accessor :report
   end
 end
