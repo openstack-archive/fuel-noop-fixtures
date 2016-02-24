aa3f3ef63a9bf84c3161ab88e0a59f7c2494022d
Dmitry Ilyin <dilyin@mirantis.com>
2016-02-24 19:53:55 +0300

    Update
    * Improve reporting
    * More comments
    * Port back file resource reporting features
    * Port back data-based testing
    * Improve filtering
    * Improve docs
    
    Change-Id: I218fce4e6012ce1171a5042eebc8ac95ba734c21

--- lib/noop/task/overrides.rb
+++ lib/noop/task/overrides.rb
@@ -1,19 +1,24 @@
 module Noop
   class Task
+    # Setup all needed override functions
     def setup_overrides
       puppet_default_settings
-      hiera_config_override
       puppet_debug_override if ENV['SPEC_PUPPET_DEBUG']
-      setup_manifest
       puppet_resource_scope_override
+      return unless file_name_spec_set?
+      hiera_config_override
+      setup_manifest
     end
 
+    # Set the current module path and the manifest file
+    # to run in this RSpec session
     def setup_manifest
       RSpec.configuration.manifest = file_path_manifest.to_s
       RSpec.configuration.module_path = Noop::Config.dir_path_modules_local.to_s
       RSpec.configuration.manifest_dir = Noop::Config.dir_path_tasks_local.to_s
     end
 
+    # Override Hiera configuration in the Puppet objects
     def hiera_config_override
       class << HieraPuppet
         def hiera
@@ -48,6 +53,7 @@ module Noop
       Hiera::Config.config = hiera_config
     end
 
+    # Ask Puppet to save the current scope reference to the task instance
     def puppet_resource_scope_override
       Puppet::Parser::Resource.module_eval do
         def initialize(*args)
@@ -60,6 +66,7 @@ module Noop
       end
     end
 
+    # Divert Puppet logs to the console
     def puppet_debug_override
       Puppet::Util::Log.level = :debug
       Puppet::Util::Log.newdestination(:console)
