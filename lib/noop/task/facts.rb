b7560ac0a7b5759b6c4623dc1478c58c560eadad
Dmitry Ilyin <dilyin@mirantis.com>
2016-08-31 13:30:47 -0500

    Misc fixes
    * Change facts structure from a Hash to a HashWithIndifferentAccess.
      It will simplyfy adding facts values and working with them.
    * Fix Gem versions reporter
    
    Change-Id: Ie28b2ea6bcd854e96cc8d9833be8adaafd689140

--- lib/noop/task/facts.rb
+++ lib/noop/task/facts.rb
@@ -1,4 +1,5 @@
 require 'yaml'
+require 'active_support/core_ext/hash/indifferent_access'
 
 module Noop
   class Task
@@ -72,7 +73,7 @@ module Noop
 
     # @return [Hash]
     def facts_data
-      facts_data = {}
+      facts_data = ActiveSupport::HashWithIndifferentAccess.new
       facts_hierarchy.each do |file_path|
         begin
           file_data = YAML.load_file file_path
