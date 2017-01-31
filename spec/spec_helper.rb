bd8096e59e094431c5b5e9364b90ea7339b58b2c
Mykyta Karpin <mkarpin@mirantis.com>
2017-01-31 13:04:16 +0200

    [puppet4] Workaround for PUP-3336
    Noop tests fail to autoload unless lib_dirs
    aren't added to LOAD_PATH.
    
    Change-Id: I7c7ee08ea345afc6f5bbe560f06ab106132288f6
    Closes-Bug: #1660579

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,11 +9,9 @@ require 'webmock/rspec'
 require_relative '../lib/noop'
 
 # Add fixture lib dirs to LOAD_PATH. Work-around for PUP-3336
-if Puppet.version < '4.0.0'
-  Noop::Config.list_path_modules.each do |path|
-    Dir["#{path}/*/lib"].entries.each do |lib_dir|
-      $LOAD_PATH << lib_dir
-    end
+Noop::Config.list_path_modules.each do |path|
+  Dir["#{path}/*/lib"].entries.each do |lib_dir|
+    $LOAD_PATH << lib_dir
   end
 end
 
